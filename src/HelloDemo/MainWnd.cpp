90c8e52c961eb6efac5012c49bd6c65dd8029636
飞鹰 <zhangQ8848@163.com>
2018-10-30 13:20:24 +0800

    init

---
+++ src/HelloDemo/MainWnd.cpp
@@ -0,0 +1,24 @@
+﻿#include "MainWnd.h"
+
+#include <commdlg.h>
+#include <fstream>
+#include <sstream>
+
+void CMainWnd::InitWindow()
+{
+    m_pBtnHello = static_cast<CButtonUI *>(m_PaintManager.FindControl(_T("btnHello")));
+}
+
+void CMainWnd::Notify(TNotifyUI &msg)
+{
+    if (msg.sType == _T("click"))
+    {
+        CDuiString name = msg.pSender->GetName();
+        if (name.CompareNoCase(_T("btnHello")) == 0)
+        {	// 点击按钮
+            MessageBox(m_hWnd, _T("您好，DuiLib_WC!!!"), _T("温馨提示"), MB_OK);
+        }
+    }
+
+    return WindowImplBase::Notify(msg);
+}
---
+++ src/HelloDemo/MainWnd.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "stdafx.h"
+#include "resource.h"
+
+using namespace DuiLib;
+
+class CMainWnd : public WindowImplBase
+{
+public:
+  virtual LPCTSTR GetWindowClassName() const { return _T("MainFrameWnd"); }
+  virtual CDuiString GetSkinFile() { return _T("main.xml"); }
+  virtual CDuiString GetSkinFolder() { return _T("skin"); }
+  virtual UILIB_RESOURCETYPE GetResourceType() const { return UILIB_ZIPRESOURCE; }
+  virtual LPCTSTR GetResourceID() const { return MAKEINTRESOURCE(IDR_ZIPRES); }
+
+  virtual void InitWindow();
+  virtual void Notify(TNotifyUI &msg);
+
+private:
+  CButtonUI *m_pBtnHello;
+};
